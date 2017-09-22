97c29fe3a71a17c97a10f27441e730f6d2d7376c
Ishan Herath <ishan@wishpond.com>
2017-09-21 18:29:49 -0700

    Initial commit

---
+++ lib/image.ex
@@ -0,0 +1,8 @@
+defmodule Identicon.Image do
+  @moduledoc """
+    Module containing struct definition for an identicon image.
+  """
+
+  defstruct hex: [], color: {}, grid: [], pixel_map: []
+
+end
